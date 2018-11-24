905321a82d3b32ac07051bf86667d618e97984a9
txiverke <txiverke@gmail.com>
2018-11-24 13:45:04 -0500

    Update v:1.0.4 - Testing in production

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import config from './config';
 
 import App from './App.jsx';
-import './css/App.css'
+import './css/App.css';
 
 const elem = document.querySelector('.root');
+
+console.log(config);
 
 const wrapApp = AppComponent => (
   <Router>

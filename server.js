c8cdeafc4e907ca67db5980cb6c2303c253433b7
txiverke <txiverke@gmail.com>
2018-11-29 11:36:08 -0500

    Update v: 1.0.6 - Testing ServiceWorker in prod

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "service-worker.js"));
+});
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'), (err) => {
     if (err) {

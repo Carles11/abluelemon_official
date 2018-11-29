95144aabfab30ec83d4e315806969d4b4d70e5a9
txiverke <txiverke@gmail.com>
2018-11-29 11:26:43 -0500

    Update v: 1.0.6 - Testing ServiceWorkers in prod

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -30,7 +30,8 @@ export default function register() {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+			const swUrl = `${PUBLIC_URL}/service-worker.js`;
+			console.log(swUrl)
       if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
         checkValidServiceWorker(swUrl);

cdc76c7bfa6cf9fdc2e4d1290fed52f71f358d79
Carles <carles@crix.design>
2022-03-13 12:08:43 -0300

    PORT env

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const compression = require('compression')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 5000

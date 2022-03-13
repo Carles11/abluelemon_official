42280d51832d93eefe42e01f2a8a0f0fac103c4b
Carles <carles@crix.design>
2022-03-13 12:03:29 -0300

    dotenv removal

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const compression = require('compression')
 const bodyParser = require('body-parser')
-require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
